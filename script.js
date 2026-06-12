ed91b994235eb4e4c93f458b3c3a54d9169fbc33
Eddy Ryansyah <eddyryansyah1612@gmail.com>
2026-06-13 00:06:57 +0700

    perf: improve accessibility and asset delivery

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function toggleMenu() {
-  const menu = document.querySelector(".menu-links");
-  const icon = document.querySelector(".hamburger-icon");
-  menu.classList.toggle("open");
-  icon.classList.toggle("open");
+const menu = document.querySelector(".menu-links");
+const menuButton = document.querySelector(".hamburger-icon");
+const menuLinks = document.querySelectorAll(".menu-links a");
+
+function closeMenu() {
+  menu.classList.remove("open");
+  menuButton.classList.remove("open");
+  menuButton.setAttribute("aria-expanded", "false");
 }
+
+function toggleMenu() {
+  const isOpen = menu.classList.contains("open");
+
+  if (isOpen) {
+    closeMenu();
+  } else {
+    menu.classList.add("open");
+    menuButton.classList.add("open");
+    menuButton.setAttribute("aria-expanded", "true");
+  }
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
